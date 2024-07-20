69442b1dfc9e3676f02f7e6bbae97100a8735d20
jamleston <jamlessston@gmail.com>
2024-07-20 09:33:59 +0200

    second task

--- second.py
+++ second.py
@@ -1,16 +1,21 @@
 import re
+from typing import Callable
 
 def generator_numbers(text: str):
-    pattern = r'^\s\d$\d\s'
-    num = re.search(pattern, text).group()
-    return num
+    pattern =  r" \d+.\d+ "
+    nums = re.findall(pattern, text)
+    for num in nums:
+        num = float(num.strip())
+        yield num
 
-def sum_profit(text: str, func: callable):
-    pass
+def sum_profit(text: str, func: Callable):
+    nums = func(text)
+    sum = 0
+    for num in nums:
+        sum = sum+num
+    return sum
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
-# total_income = sum_profit(text, generator_numbers)
-# print(f"Загальний дохід: {total_income}")
 
-a = generator_numbers(text)
-print(a)
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
