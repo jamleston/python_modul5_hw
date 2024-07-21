9ce2270038066c700709d8b0f90747d95121edb0
jamleston <jamlessston@gmail.com>
2024-07-21 07:50:01 +0200

    add 2 func task 3

--- third/third.py
+++ third/third.py
@@ -1,2 +1,28 @@
+from pathlib import Path
+
 def parse_log_line(line: str) -> dict:
-    pass
+    list = line.split()
+    dict = {}
+    dict['date'] = list[0]
+    dict['time'] = list[1]
+    dict['type'] = list[2]
+    dict['message'] = ' '.join(list[3:])
+    return dict
+
+def load_logs(file_path: str) -> list:
+    try:
+        with open(file_path, "r") as file:
+            lines = [el.strip() for el in file.readlines()]
+            list_of_logs = []
+            for line in lines:
+                list_of_logs.append(parse_log_line(line))
+            return list_of_logs
+    except (TypeError, FileNotFoundError):
+        answer = 'path is not correct'
+        print(answer)
+        return False
+
+
+path = Path('third//log.txt')
+a = load_logs(path)
+print(a)
